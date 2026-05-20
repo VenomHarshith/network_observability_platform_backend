42892b0247226e2be9664830247c2c154e3e86f4
SriHarshith <jangamsriharshith@gmail.com>
2026-05-21 00:51:05 +0530

    fix frontend backend api urls

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,29 +1,29 @@
 export const getMetrics = async () => {
-  const res = await fetch("http://127.0.0.1:8000/api/metrics/latest");
+  const res = await fetch("https://network-observability-api.onrender.com/api/metrics/latest");
   return { data: await res.json() };
 };
 
 export const getAlerts = async () => {
-  const res = await fetch("http://127.0.0.1:8000/api/alerts");
+  const res = await fetch("https://network-observability-api.onrender.com/api/alerts");
   return { data: await res.json() };
 };
 
 export const getProtocols = async () => {
-  const res = await fetch("http://127.0.0.1:8000/api/protocols");
+  const res = await fetch("https://network-observability-api.onrender.com/api/protocols");
   return res.ok ? await res.json() : [];
 };
 
 export const getTopTalkers = async () => {
-  const res = await fetch("http://127.0.0.1:8000/api/top-talkers");
+  const res = await fetch("https://network-observability-api.onrender.com/api/top-talkers");
   return res.ok ? await res.json() : { src: [], dst: [] };
 };
 
 export const getTopology = async () => {
-  const res = await fetch("http://127.0.0.1:8000/api/topology");
+  const res = await fetch("https://network-observability-api.onrender.com/api/topology");
   return res.ok ? await res.json() : [];
 };
 
 export const getAlertDetails = async () => {
-  const res = await fetch("http://127.0.0.1:8000/api/alerts/details");
+  const res = await fetch("https://network-observability-api.onrender.com/api/alerts/details");
   return res.ok ? await res.json() : {};
 };
